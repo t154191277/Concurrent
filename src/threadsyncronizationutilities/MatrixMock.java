edabc50fc88c67f27b95ddd29fae05c9dc54d709
wml0ve <154191277@qq.com>
2016-06-09 22:22:36 +0800

    learn synchroized utils part two

---
+++ src/threadsyncronizationutilities/MatrixMock.java
@@ -0,0 +1,33 @@
+package threadsyncronizationutilities;
+
+import java.util.Random;
+
+public class MatrixMock {
+	private int data[][];
+
+	public MatrixMock(int size, int length, int number) {
+		int counter = 0;
+		data = new int[size][length];
+		Random random = new Random();
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < length; j++) {
+				data[i][j] = random.nextInt(10);
+				if (data[i][j] == number) {
+					counter++;
+				}
+			}
+		}
+		// 译者注：把字符串里的number改为%d.
+		System.out.printf(
+				"Mock: There are %d ocurrences of number in generated data.\n",
+				counter, number);
+	}
+
+	public int[] getRow(int row) {
+		if ((row >= 0) && (row < data.length)) {
+			return data[row];
+		}
+		return null;
+	}
+
+}
--- src/threadsyncronizationutilities/RTest.java
+++ src/threadsyncronizationutilities/RTest.java
@@ -1,12 +1,13 @@
 package threadsyncronizationutilities;
 
+import java.util.concurrent.CyclicBarrier;
+
 import org.apache.log4j.chainsaw.Main;
 import org.junit.Test;
 
-
 public class RTest {
-	
-	@Test
+
+	// @Test
 	public void testBinarySemaphores() {
 		PrintQueue printQueue = new PrintQueue();
 		Thread thread[] = new Thread[10];
@@ -17,16 +18,44 @@ public class RTest {
 			thread[i].start();
 		}
 	}
-	
+
 	public static void main(String[] args) {
-		PrintQueue2 printQueue = new PrintQueue2();
-		Thread thread[] = new Thread[10];
+		
+	}
+
+	// @Test
+	public void testCountDownLatch() {
+		Videoconference conference = new Videoconference(10);
+		Thread threadConference = new Thread(conference);
+		threadConference.start();
 		for (int i = 0; i < 10; i++) {
-			thread[i] = new Thread(new Job(printQueue), "Thread" + i);
+			Participant p = new Participant(conference, "Participant " + i);
+			Thread t = new Thread(p);
+			t.start();
 		}
-		for (int i = 0; i < 10; i++) {
-			thread[i].start();
+	}
+
+	@Test
+	public void testCyclicBarrier() {
+		final int ROWS = 10000;
+		final int NUMBERS = 1000;
+		final int SEARCH = 5;
+		final int PARTICIPANTS = 5;
+		final int LINES_PARTICIPANT = 2000;
+		MatrixMock mock = new MatrixMock(ROWS, NUMBERS, SEARCH);
+		Results results = new Results(ROWS);
+		Grouper grouper = new Grouper(results);
+//		 创建 CyclicBarrier 对象，名为 barrier。此对象会等待5个线程。当此线程结束后，它会执行前面创建的 Grouper 对象。
+		CyclicBarrier barrier = new CyclicBarrier(PARTICIPANTS, grouper);
+		Searcher searchers[] = new Searcher[PARTICIPANTS];
+		for (int i = 0; i < PARTICIPANTS; i++) {
+			searchers[i] = new Searcher(i * LINES_PARTICIPANT,
+					(i * LINES_PARTICIPANT) + LINES_PARTICIPANT, mock, results,
+					5, barrier);
+			Thread thread = new Thread(searchers[i]);
+			thread.start();
 		}
+		System.out.printf("Main: The main thread has finished.\n");
 	}
 
 }
